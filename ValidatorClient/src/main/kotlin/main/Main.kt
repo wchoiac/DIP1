81cf14b739be06ca2f2e02af6624fa86b364e2c2
gusrb <gusrb415@gmail.com>
2019-04-14 01:40:47 +0900

    Wrong Previous Commit, Fall Back to original

--- ValidatorClient/src/main/kotlin/main/Main.kt
+++ ValidatorClient/src/main/kotlin/main/Main.kt
@@ -1,11 +1,17 @@
 package main
 
 import javafx.application.Application
+import javafx.stage.Screen
 import javafx.stage.Stage
+import viewmodel.Config
 import viewmodel.SceneManager
 
 class Main : Application() {
     override fun start(primaryStage: Stage) {
+        Config.WIDTH = Screen.getScreens().first().visualBounds.width
+        Config.HEIGHT = Screen.getScreens().first().visualBounds.height
+        Config.IMAGE_WIDTH = Config.WIDTH * 0.25
+        Config.IMAGE_HEIGHT = Config.HEIGHT * 0.25
         SceneManager.stage = primaryStage
         SceneManager.showLogInScene()
     }
